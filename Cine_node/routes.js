0fee755b92230006a044089477c79d514c3b2b38
Brendaxd <47357641+Brendaxd@users.noreply.github.com>
2019-02-26 23:15:21 +0100

    Add files via upload

---
+++ Cine_node/routes.js
@@ -0,0 +1,14 @@
+var controllerpelicula = require('./controllerpelicula.js');
+module.exports = function(app){
+	var clasepelicula = new controllerpelicula();
+	app.post('/api/nuevapelicula',clasepelicula.Guardar);
+	app.post('/api/modificapelicula',clasepelicula.Modificar);
+	app.post('/api/eliminapelicula',clasepelicula.Eliminar);
+	app.post('/api/seleccionapelicula',clasepelicula.Seleccionartodos);
+	app.post('/api/seleccionapeliculaporfecha',clasepelicula.Seleccionarporfecha);
+	app.post('/api/seleccionapeliculaporid',clasepelicula.Seleccionarporid);
+	app.post('/api/seleccionapeliculapornombre',clasepelicula.Seleccionarpornombre);
+	app.get('*',function(req,res){
+		 res.sendfile('./login.html');
+	});
+};
